0d2421b3ec8f8df95b6922609804e694310d9287
taha <tahasrlky2@gmail.com>
2025-06-29 15:04:23 +0300

    توضیح تغییرات

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const App: React.FC = () => {
             </BlogProvider>
           </InstructorProvider>
         </CourseProvider>
-      </AuthProvider>
+      </AuthProvider> 
     </div>
   );
 };
